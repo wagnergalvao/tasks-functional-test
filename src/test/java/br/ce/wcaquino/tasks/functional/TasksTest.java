8fbff9662c2772c0ff5549b3ad6074b9479bb8db
Wagner Galvao <wagner.galvao@yaman.com.br>
2020-06-30 16:15:09 -0300

    Class 48: Commitando testes funcionais

---
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -0,0 +1,125 @@
+package br.ce.wcaquino.tasks.functional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TasksTest {
+	public static DateTimeFormatter task = DateTimeFormatter.ofPattern("A EEEE");
+	public static DateTimeFormatter dueDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	public WebDriver acessarAplicacao() {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8081/tasks/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+
+	@Test
+	public void deveSalvarTarefaComSucesso() {
+		LocalDateTime dataTarefa = LocalDateTime.now();
+		WebDriver driver = acessarAplicacao();
+
+		try {
+			// Clicar no botão Add Todo
+			driver.findElement(By.id("addTodo")).click();
+
+			// Escrever a descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium " + dataTarefa.format(task));
+
+			// Escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys(dataTarefa.format(dueDate));
+
+			// Clicar no botão Save
+			driver.findElement(By.id("saveButton")).click();
+
+			// Validar mensagem de Sucesso
+			String mensagem = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", mensagem);
+		} finally {
+			// Fechar o browser
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		LocalDateTime dataTarefa = LocalDateTime.now();
+		WebDriver driver = acessarAplicacao();
+
+		try {
+			// Clicar no botão Add Todo
+			driver.findElement(By.id("addTodo")).click();
+
+			// Escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys(dataTarefa.format(dueDate));
+
+			// Clicar no botão Save
+			driver.findElement(By.id("saveButton")).click();
+
+			// Validar mensagem de Erro
+			String mensagem = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the task description", mensagem);
+		} finally {
+			// Fechar o browser
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		LocalDateTime dataTarefa = LocalDateTime.now();
+		WebDriver driver = acessarAplicacao();
+
+		try {
+			// Clicar no botão Add Todo
+			driver.findElement(By.id("addTodo")).click();
+
+			// Escrever a descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium " + dataTarefa.format(task));
+
+			// Clicar no botão Save
+			driver.findElement(By.id("saveButton")).click();
+
+			// Validar mensagem de Erro
+			String mensagem = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the due date", mensagem);
+		} finally {
+			// Fechar o browser
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaComDataPassada() {
+		LocalDateTime dataTarefa = LocalDateTime.now().minusDays(1);
+		WebDriver driver = acessarAplicacao();
+
+		try {
+			// Clicar no botão Add Todo
+			driver.findElement(By.id("addTodo")).click();
+
+			// Escrever a descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium " + dataTarefa.format(task));
+
+			// Escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys(dataTarefa.format(dueDate));
+
+			// Clicar no botão Save
+			driver.findElement(By.id("saveButton")).click();
+
+			// Validar mensagem de Erro
+			String mensagem = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Due date must not be in past", mensagem);
+		} finally {
+			// Fechar o browser
+			driver.quit();
+		}
+	}
+}
